f1f0255c9df66da40c6cc83f647a3ecb1258af68
Mub1532 <mubkhan644@gmail.com>
2026-08-08 16:34:10 +0100

    add the schema templates, require env

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,21 @@ import { join } from 'node:path'
 import AutoLoad, { AutoloadPluginOptions } from '@fastify/autoload'
 import { FastifyPluginAsync, FastifyServerOptions } from 'fastify'
 
-export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {}
+export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> { }
 
 const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts
 ): Promise<void> => {
+
+  const reqENV = ['API_KEY', 'IMMICH_API_URL', 'IMMICH_API_KEY', 'TZ']
+
+  const missingENV = reqENV.filter((key) => !process.env[key])
+
+  if (missingENV.length > 0) {
+    fastify.log.error(`Please fill out the ENV for: ${missingENV.join(', ')}`)
+    process.exit(1)
+  }
 
 
   fastify.addHook('onRequest', async (request, reply) => {
